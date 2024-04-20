d938db15322ba88e5f461127b76aace2af4acad1
emil <emilquluzade31.05@gmail.com>
2024-04-20 13:45:05 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+let allAddBtn = document.querySelectorAll(".btn-primary");
+let allDelBtn = document.querySelectorAll(".btn-danger");
+let basketcount = document.querySelector(".basketcount");
+let table = document.querySelector(".table");
+
+let basket = JSON.parse(localStorage.getItem("basket"));
+
+if (localStorage.getItem("basket") == null) {
+  localStorage.setItem("basket", JSON.stringify([]));
+}
+function getBasketCount() {
+  if (localStorage.getItem("basket") != null) {
+    let basket = JSON.parse(localStorage.getItem("basket"));
+    basketcount.innerText = basket.length;
+  }
+}
+
+for (const btn of allAddBtn) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let Id = btn.parentElement.parentElement.getAttribute("data-id");
+    let Name = btn.previousElementSibling.previousElementSibling.innerText;
+    let Image = btn.parentElement.previousElementSibling.getAttribute("src");
+
+    if (localStorage.getItem("basket") == null) {
+      localStorage.setItem("basket", JSON.stringify([]));
+    }
+
+    let basket = JSON.parse(localStorage.getItem("basket"));
+
+    let selectProduct = basket.find((p) => p.id == Id);
+
+    if (selectProduct === undefined) {
+      basket.push({
+        id: Id,
+        count: 1,
+        name: Name,
+        image: Image,
+      });
+    } else {
+      selectProduct.count++;
+    }
+
+    localStorage.setItem("basket", JSON.stringify(basket));
+    getBasketCount();
+  });
+}
+for (const product of basket) {
+  table.innerHTML += `
+    <tr>
+      <td>
+       <img width="50px" src="${product.image}"   alt="">
+      </td>
+      <td>${product.name}</td>
+      <td> ${product.count}</td>
+    </tr>
+    `;
+}
