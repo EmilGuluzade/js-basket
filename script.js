71575afe90f0249e29b9904ec7f7f8a3f5e2a804
emil <emilquluzade31.05@gmail.com>
2024-04-21 15:08:00 +0400

    commit

--- script.js
+++ script.js
@@ -1,58 +1,93 @@
-let allAddBtn = document.querySelectorAll(".btn-primary");
+  const allAddBtn = document.getElementsByClassName("btn-primary");
+ 
 
-let basketCount = document.querySelector(".basketcount");
-let table = document.querySelector(".table");
+  let basketCount = document.querySelector(".basketcount");
+  let table = document.querySelector(".table");
 
-let basket = JSON.parse(localStorage.getItem("basket"));
+  let basket = JSON.parse(localStorage.getItem("basket"));
 
-if (localStorage.getItem("basket") == null) {
-  localStorage.setItem("basket", JSON.stringify([]));
-}
-function getBasketCount() {
-  if (localStorage.getItem("basket") != null) {
-    let basket = JSON.parse(localStorage.getItem("basket"));
-    basketCount.innerText = basket.length;
+  if (localStorage.getItem("basket") == null) {
+    localStorage.setItem("basket", JSON.stringify([]));
   }
-}
+ 
+  for (const btn of allAddBtn) {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      let Id = btn.parentElement.parentElement.getAttribute("data-id");
+      let Name = btn.previousElementSibling.previousElementSibling.innerText;
+      let Image = btn.parentElement.previousElementSibling.getAttribute("src");
 
-for (const btn of allAddBtn) {
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
-    let Id = btn.parentElement.parentElement.getAttribute("data-id");
-    let Name = btn.previousElementSibling.previousElementSibling.innerText;
-    let Image = btn.parentElement.previousElementSibling.getAttribute("src");
+      let basket = JSON.parse(localStorage.getItem("basket"));
 
-    if (localStorage.getItem("basket") == null) {
-      localStorage.setItem("basket", JSON.stringify([]));
+      let selectProduct = basket.find((p) => p.id == Id);
+
+      if (selectProduct === undefined) {
+        basket.push({
+          id: Id,
+          count: 1,
+          name: Name,
+          image: Image,
+        });
+      } else {
+        selectProduct.count++;
+      }
+
+      localStorage.setItem("basket", JSON.stringify(basket));
+      getBasketCount();
+    });
+  }
+  
+
+  // function attachDelete() {
+  //   let allDelBtn = document.querySelectorAll(".btn-danger");
+  //   for (const btnDel of allDelBtn) {
+  //     btnDel.addEventListener("click", function (e) {
+  //       e.preventDefault();
+  //       console.log("slaam");
+  //       let Name =
+  //         btnDel.parentElement.previousElementSibling.previousElementSibling
+  //           .innerText;
+  
+  //       let basket = JSON.parse(localStorage.getItem("basket"));
+  
+  //       let target = basket.find((p) => p.name == Name);
+  //       let indexOfTarget = basket.indexOf(target);
+  
+  //       if (target.count > 1) {
+  //         target.count--;
+  //       } else {
+  //         basket.splice(indexOfTarget, 1);
+  //       }
+  // console.log(target.count)
+  //       localStorage.setItem("basket", JSON.stringify(basket));
+  //       basket = JSON.parse(localStorage.getItem("basket"));
+  //       getBasketCount(basket);
+  //     });
+  //   }
+  // }
+ function getBasketCount() {
+    if (localStorage.getItem("basket") != null) {
+      let basket = JSON.parse(localStorage.getItem("basket"));
+      let countSum = basket.reduce((acc, el) => acc + el.count, 0);
+      basketCount.innerText = countSum;
     }
+  }
+  getBasketCount();
 
-    let basket = JSON.parse(localStorage.getItem("basket"));
+  
+  for (const product of basket) {
+    table.innerHTML += `
+      <tr>
+        <td>
+        <img width="50px" src="${product.image}"   alt="">
+        </td>
+        <td>${product.name}</td>
+        <td> ${product.count}</td>
+        <td> <button  class="btn btn-danger">delete</button></td>
+      </tr>
+      `;
+        
+  }
+attachDelete();
 
-    let selectProduct = basket.find((p) => p.id == Id);
 
-    if (selectProduct === undefined) {
-      basket.push({
-        id: Id,
-        count: 1,
-        name: Name,
-        image: Image,
-      });
-    } else {
-      selectProduct.count++;
-    }
-
-    localStorage.setItem("basket", JSON.stringify(basket));
-    getBasketCount();
-  });
-}
-for (const product of basket) {
-  table.innerHTML += `
-    <tr>
-      <td>
-       <img width="50px" src="${product.image}"   alt="">
-      </td>
-      <td>${product.name}</td>
-      <td> ${product.count}</td>
-    </tr>
-    `;
-}
